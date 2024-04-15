6756070ce4ba5566447c0292154369f8d7ec6fbc
Roman <Alimov.ramon@gmail.com>
2024-04-15 17:21:04 +0300

    done test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent, FC, useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Color {
+  colorName: string;
+}
+interface Param {
+  id: number;
+  name: string;
+  type: "string";
+}
+interface ParamValue {
+  paramId: number;
+  value: string;
+}
+interface Model {
+  paramsValue: ParamValue[];
+  colors: Color[];
+}
+interface Props {
+  params: Param[];
+  model: Model;
 }
 
-export default App;
+const params: Param[] = [
+  { id: 1, name: "Назначение", type: "string" },
+  { id: 2, name: "Длина", type: "string" },
+];
+const model: Model = {
+  paramsValue: [
+    { paramId: 1, value: "Повседневное" },
+    { paramId: 2, value: "Макси" },
+  ],
+  colors: [],
+};
+
+const Main: FC<Props> = ({ model, params }) => {
+  const [state, setState] = useState({ model, params });
+
+  const getModel = () => {
+    return state.model.paramsValue;
+  };
+
+  const onChangeValue = (id: number, e: ChangeEvent<HTMLInputElement>) => {
+    setState({
+      ...state,
+      model: {
+        ...state.model,
+        paramsValue: state.model.paramsValue.map((model) =>
+          model.paramId === id
+            ? { ...model, value: e.currentTarget.value }
+            : model
+        ),
+      },
+    });
+  };
+
+  return (
+    <div className="App">
+      <div>
+        {state.params.map((param) => (
+          <div key={param.id}>
+            <div>{param.name}</div>{" "}
+          </div>
+        ))}
+      </div>
+      <div>
+        {state.model.paramsValue.map((m) => (
+          <div key={m.paramId}>
+            <input
+              value={m.value}
+              onChange={(e) => onChangeValue(m.paramId, e)}
+            />
+          </div>
+        ))}
+      </div>
+      <div>
+        <button onClick={() => console.log(getModel())}>get model</button>
+      </div>
+    </div>
+  );
+};
+
+export const App = () => {
+  return <Main model={model} params={params} />;
+};
